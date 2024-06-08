0363e0ca238ed5c0e98fe6fb4f4ad29e16da887a
Denis Ansah <denis.ansah@yahoo.com>
2024-06-07 23:55:15 -0400

    Initial Commit

---
+++ Indigo/IndigoApp.swift
@@ -0,0 +1,17 @@
+//
+//  IndigoApp.swift
+//  Indigo
+//
+//  Created by Denis on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct IndigoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
