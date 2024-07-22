09576a2cf971262ad1ccc27c6e43640d6853125f
Denis Ansah <denis.ansah@yahoo.com>
2024-07-21 23:16:58 -0400

    Initial Commit

---
+++ Indigo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Indigo
+//
+//  Created by Denis Ansah on 7/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Indigo/IndigoApp.swift
@@ -0,0 +1,17 @@
+//
+//  IndigoApp.swift
+//  Indigo
+//
+//  Created by Denis Ansah on 7/21/24.
+//
+
+import SwiftUI
+
+@main
+struct IndigoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
