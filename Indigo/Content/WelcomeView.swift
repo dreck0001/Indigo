24f8accedbcb31b1164e50ee1f0e28056d0fb96b
Denis Ansah <denis.ansah@yahoo.com>
2024-07-24 00:07:08 -0400

    Added a welcome screen with a brief tutorial. Conformed to the Indigo color scheme to stay in tune with the name of the app.

---
+++ Indigo/Content/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  Indigo
+//
+//  Created by Denis Ansah on 7/23/24.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
